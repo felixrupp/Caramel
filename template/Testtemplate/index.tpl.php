4ff366fd20a4a7d0473b08efbe9616f7f30d1ef9
Felix Rupp <kontakt@felixrupp.com>
2012-01-29 15:31:35 +0100

    DTDs included in XML-Files Template modifications backend and testtemplate

--- template/Testtemplate/index.tpl.php
+++ template/Testtemplate/index.tpl.php
@@ -1,31 +1,37 @@
 <!-- This is our test template -->
 
-<div id="menu">
-	<ul>
-	 <li class="logo">Caramel</li>
-  <?php foreach($navigation as $naventry) { ?>
-			
-		<li><?php echo $naventry["link"]; ?>
-		
-		<?php 
-		if(isset($naventry["subpages"]) && count($naventry["subpages"]>0)) {?>
-			<ul>
-		<?php foreach($naventry["subpages"] as $subentry) {?>
-				<li><?php echo $subentry["link"]; ?></li>
-		<?php } ?>
-			</ul>
-		<?php } ?>
-		  </li>
-	<?php } ?>
-	</ul>
-</div>
+<div id="site">
+  <h1>CARAMEL CMS</h1>
+	<div id="wrapper">
+	
+	  <div id="content">
+	   <?php echo $content; ?>
+    </div>
+    
+	</div>
+	
+  <div id="menu">
+  	<ul>
+  	 <li class="logo">Caramel</li>
+    <?php foreach($navigation as $naventry) { ?>
+  			
+  		<li><?php echo $naventry["link"]; ?>
+  		
+  		<?php 
+  		if(isset($naventry["subpages"]) && count($naventry["subpages"]>0)) {?>
+  			<ul>
+  		<?php foreach($naventry["subpages"] as $subentry) {?>
+  				<li><?php echo $subentry["link"]; ?></li>
+  		<?php } ?>
+  			</ul>
+  		<?php } ?>
+  		  </li>
+  	<?php } ?>
+  	</ul>
+  </div>
 
+  <div id="footer">
+  	<div class="footer_left"><?php echo $footer; ?></div>
+  </div>
 
-<div id="content">
-	<?php echo $content; ?>
-</div>
-
-
-<div id="footer">
-	<div class="footer_left"><?php echo $footer; ?></div>
 </div>
