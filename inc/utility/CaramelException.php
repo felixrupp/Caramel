356002df6987853c2db5428b6f5f4318ff5b3217
Felix Rupp <kontakt@felixrupp.com>
2012-06-05 22:29:44 +0200

    Nicer error messages in CaramelException

--- inc/utility/CaramelException.php
+++ inc/utility/CaramelException.php
@@ -5,6 +5,9 @@
  * @subpackage utility
  */
 
+
+require_once BASEDIR.'/inc/model/ConfigurationModel.php';
+
 /**
  *
  * CaramelException class
@@ -25,10 +28,15 @@ class CaramelException extends Exception {
 	 * @var Array $_codeArray Array which assigns messages to errorcodes 
 	 */
 	private $_codeArray = array(
-		10 => "XML-Error",
-		11 => "XML-file could not be loaded",
+		10 => "XML-Error!",
+		11 => "XML-file could not be loaded!",
 		66 => "Bcrypt is not supported on this server!"
 	);
+	
+	/**
+	 * @var String $_adminEmail eMail adress of the admin
+	 */
+	private $_adminEmail = "";
 	
 	
 	/**
@@ -38,6 +46,11 @@ class CaramelException extends Exception {
 	 * @return void
 	 */
 	public function __construct($caramelCode) {
+		
+		# Get Configurator
+		$this->_config = ConfigurationModel::getConfigurationModel();
+	
+		$this->_adminEmail = $this->_config->getAdminConfigStringAction("ADMIN_EMAIL");
 		
 		$this->code = $caramelCode;
 		$this->message = $this->_codeArray[$caramelCode];
@@ -52,8 +65,7 @@ class CaramelException extends Exception {
 	 */
 	public function getDetails() {
 		
-		print($this->message."<br>in file: ".$this->file." at line: ".$this->line."<br><br>Please contact the administrator of this page.");
-		exit;
+		print("<div style=\"margin: 20px auto; width: 80%; padding:12px 16px; border: 2px solid #ec4040; background-color: #fb8b8b; color:white; font-weight: bold; font-family: 'Courier New', Courier, monospace; font-size: 1.2em;\"><p>Error:</p><p>".$this->message."<br>raised in file: ".$this->file." at line: ".$this->line."</p><p>Please contact the administrator of this page via ".$this->_adminEmail.".</p></div>");
 		
 	} // End of method declaration
 		
